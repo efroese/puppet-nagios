75d57d35a5b0a256c48ba7b02e8ba6f5c1a7d84d
Erik Froese <erik.froese@gmail.com>
2012-04-24 16:46:27 -0700

    Fix the cpu load check command templating.

--- manifests/service/nrpe/load.pp
+++ manifests/service/nrpe/load.pp
@@ -4,13 +4,13 @@ define nagios::service::nrpe::load (
   nagios::service { "nrpe-check_load":
     ensure => $ensure,
     host_name => $::fqdn,
-    check_command => "check_nrpe!check_load",
+    check_command => 'check_nrpe!check_load',
     service_description => "CPU load check",
   }
 
   $pc = $::processorcount
   nrpe::command { "check_load":
-      cmd => "/usr/lib64/nagios/plugins/check_load -w <%= pc * 3 %>,<%= pc * 2 %>,<%= pc * 2 %> -c <%= pc * 5 %>,<%= pc * 4 %>,<%= pc * 3 %>",
+      cmd => inline_template("/usr/lib64/nagios/plugins/check_load -w <%= Integer(pc) * 3 %>,<%= Integer(pc) * 2 %>,<%= Integer(pc) * 2 %> -c <%= Integer(pc) * 5 %>,<%= Integer(pc) * 4 %>,<%= Integer(pc) * 3 %>"),
   }
       
 }
